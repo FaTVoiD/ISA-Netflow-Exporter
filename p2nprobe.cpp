a0b584a4bcd8421ba8970e09b47235498ed788a8
FaTVoiD <m.belovec@gmail.com>
2024-11-17 14:05:40 +0100

    Added packet filtering and array functions

--- p2nprobe.cpp
+++ p2nprobe.cpp
@@ -2,6 +2,11 @@
 
 int check_timeout_arg(char *arg);
 
+void array_init(flow_array buffer);
+void array_add(flow_array buffer);
+void array_delete(flow_array buffer, flow_t flow);
+int check_flow_equality(flow_t f1, flow_t f2);
+
 int main(int argc, char *argv[])
 {
     int a_arg_index = -1;
@@ -86,23 +91,36 @@ int main(int argc, char *argv[])
         i_value = atoi(argv[i_arg_index]);
     }
 
-    struct pcap_pkthdr header;
+    struct pcap_pkthdr pcap_header;
     char pcap_error_buffer[PCAP_ERRBUF_SIZE];
 
     // Open file
-    pcap_t *handle = pcap_open_offline(file, pcap_error_buffer);
-    if (handle == NULL)
+    pcap_t *pcap_handle = pcap_open_offline(file, pcap_error_buffer);
+    if (pcap_handle == NULL)
     {
         cout << "Can't open specified PCAP file!\n";
         return -1;
     }
 
+    struct bpf_program filter;
+    if (pcap_compile(pcap_handle, &filter, "tcp", 0, 0) == -1)
+    {
+        cout << "Function call pcap_compile() unsuccessful!\n";
+        return -1;
+    }
+
+    if (pcap_setfilter(pcap_handle, &filter) == -1)
+    {
+        cout << "Function call pcap_setfilter() unsuccessful!\n";
+        return -1;
+    }
+
     const u_char *packet;
-    while ((packet = pcap_next(handle, &header)) != NULL)
+    while ((packet = pcap_next(pcap_handle, &pcap_header)) != NULL)
     {
     }
 
-    pcap_close(handle);
+    pcap_close(pcap_handle);
     return 0;
 }
 
@@ -118,6 +136,67 @@ int check_timeout_arg(char *arg)
         {
             return 1;
         }
+    }
+    return 0;
+}
+
+void array_init(flow_array buffer)
+{
+    buffer->array = (flow_t)malloc(buffer->size * sizeof(struct flow_t));
+    if (buffer->array == NULL)
+    {
+        printf("Array initialization unsuccessful!");
+        exit(-1);
+    }
+}
+
+void array_add(flow_array buffer, flow_t add_flow)
+{
+    buffer->array[index] = add_flow;
+    index++;
+
+    // Check for array fullness
+    if (index == size)
+    {
+        size = 2 * size;
+        buffer->array = (flow_t)realloc(buffer->array, buffer->size * sizeof(struct flow_t));
+        if (buffer->array == NULL)
+        {
+            printf("Array realloc unsuccessful!");
+            exit(-1);
+        }
+    }
+}
+
+void array_delete(flow_array buffer, flow_t delete_flow)
+{
+    flow_t empty_flow;
+    for (int i = 0; i < buffer->index; i++)
+    {
+        if (check_flow_equality(buffer->array[i], delete_flow))
+        {
+            for (int j = i; j < buffer->index; j++)
+            {
+                if (j + 1 < buffer->index)
+                {
+                    buffer->array[j] = buffer->array[j + 1];
+                }
+                else
+                {
+                    buffer->array[j] = empty_flow;
+                }
+            }
+            buffer->index--;
+            return;
+        }
+    }
+}
+
+int check_flow_equality(flow_t f1, flow_t f2)
+{
+    if (f1.src == f2.src && f1.dst == f2.dst && f1.src_port == f2.src_port && f1.dst_port == f2.dst_port)
+    {
+        return 1;
     }
     return 0;
 }
